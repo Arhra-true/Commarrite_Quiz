72040cae697184da2f8d33729d2f3fbc9d301fe8
Arhra-true <ryangerbal12@hotmail.co.uk>
2025-09-22 21:41:47 +0100

    Rebuild quiz
    Rebuilt the quiz using Js to populate the HTML elements after learning the hard way.

--- questions/questions.js
+++ questions/questions.js
@@ -1,209 +1,137 @@
-// Create javascript function to store the selected answer and record user score.
-//create variable to store user score, increment when correct answer selected
-let userScore = 0;
- 
-/*
-const answer1 = (document.querySelector('input[name="question1"]:checked').value === 'a')
-console.log(answer1);
 
-const answer2 = ;
-const answer3 = ;
-const answer4 = ;
-const answer5 = ;
-const answer6 =;
-const answer7 = ;
-const answer8 =;
-const answer9 = ;
-const answer10 = ;
-
-console.log(answer1)
-
-document.querySelector('input[name="question1"]:checked').value === 'a'
-const correctAnswers = placeholder; 
-*/
-
-
-
-/* Each of the following functions will use querySelector to find the first element that matches the name and 
-value for each correctly chosen answer and it's value. If the element name and value match it will
-update the user score and for ensuring the code works, it will log this to the console each time. I have 
-implemented a feature to disable the question regardless of which answer is submitted. This ensures
-the user can't swap their chosen answer or keep clicking submit and potentially calling the ++ on userScore each
-time (the button disables itself in the HTML file). */
-
-function submitAnswer () {
-    if (document.querySelector('input[name="question1"]:checked').value === 'a') {  
-    userScore++;
-    console.log(userScore); 
-  }
-  const answers = document.querySelectorAll('input[name="question1"]');
- answers.forEach(answer => {
-   answer.disabled = true;
- }); 
-}
-
-///
-
-function submitAnswer2 () {
-    if (document.querySelector('input[name="question2"]:checked').value === 'c') {  
-    userScore++;
-    console.log(userScore);
-  }
-  const answers = document.querySelectorAll('input[name="question2"]');
- answers.forEach(answer => {
-   answer.disabled = true;
- });
-}
-
-///
-
-function submitAnswer3 () {
-    if (document.querySelector('input[name="question3"]:checked').value === 'b') {  
-    userScore++;
-    console.log(userScore); 
-  }
-  const answers = document.querySelectorAll('input[name="question3"]');
- answers.forEach(answer => {
-   answer.disabled = true;
- });
-}
-
-///
-
-function submitAnswer4 () {
-    if (document.querySelector('input[name="question4"]:checked').value === 'd') {  
-    userScore++;
-    console.log(userScore); 
-  }
-  const answers = document.querySelectorAll('input[name="question4"]');
- answers.forEach(answer => {
-   answer.disabled = true;
- });
-}
-
-///
-
-function submitAnswer5 () {
-    if (document.querySelector('input[name="question5"]:checked').value === 'b') {  
-    userScore++;
-    console.log(userScore); 
-  }
-  const answers = document.querySelectorAll('input[name="question5"]');
- answers.forEach(answer => {
-   answer.disabled = true;
- }); 
-}
-
-///
-
-function submitAnswer6 () {
-    if (document.querySelector('input[name="question6"]:checked').value === 'd') {  
-    userScore++;
-    console.log(userScore); 
-  }
-  const answers = document.querySelectorAll('input[name="question6"]');
- answers.forEach(answer => {
-   answer.disabled = true;
- });
-}
-
-///
-
-function submitAnswer7 () {
-    if (document.querySelector('input[name="question7"]:checked').value === 'b') {  
-    userScore++;
-    console.log(userScore); 
-  }
-  const answers = document.querySelectorAll('input[name="question7"]');
- answers.forEach(answer => {
-   answer.disabled = true;
- });
-}
-
-///
-
-function submitAnswer8 () {
-    if (document.querySelector('input[name="question8"]:checked').value === 'a') {  
-    userScore++;
-    console.log(userScore); 
-  }
-  const answers = document.querySelectorAll('input[name="question8"]');
- answers.forEach(answer => {
-   answer.disabled = true;
- });
-}
-///
-
-function submitAnswer9 () {
-    if (document.querySelector('input[name="question9"]:checked').value === 'c') {  
-    userScore++;
-    console.log(userScore); 
-  }
-  const answers = document.querySelectorAll('input[name="question9"]');
- answers.forEach(answer => {
-   answer.disabled = true;
- });
-}
-
-///
-
-function submitAnswer10 () {
-    if (document.querySelector('input[name="question10"]:checked').value === 'c') {  
-    userScore++;
-    console.log(userScore); 
-  }
-  const answers = document.querySelectorAll('input[name="question10"]');
- answers.forEach(answer => {
-   answer.disabled = true;
- });
-}
-
-
-
-
-/* Decided to disbale the button after use, this way the answer is locked in and it stops repeated use of the button.
-
-
-/* Function works as intended, no longer increments each time. 
-I need to nest the variables locally for each question lest conflcits arise.
-The button will need to lock the question otherwise the user could select each answer and the score will increment when they click submit for each option
-*/  
-
-
-
-
-
-// old code //
-
-
-/*const selectAnswer = () => {
-    if (document.querySelector('input[name="question1"]:checked').value === 'a') {
-      userScore++; //increment score if correct answer selected}
-     // console.log(userScore); Function works as intended, but will increment the score each time the user clicks on the answer.
-}
-};
-
-console.log(userScore);
-
-//create function to lock question once answer submitted and auto-scroll to next question */
-
-
-
-
-
-
-const quizQuestions = [
+const questions = [ //array of questions that are each an object containing an array that also hs objects with two properties.
   {
   question: "What is the one and only rule in the dark city?",
-  answers: {
-    A: "No psykers are permitted in the dark city!",
-    B: "There are no rules in the dark city, fool!",
-    C: "Thou shall not steal.",
-    D: "Asdrubael Vect, the living muse and supreme overlord, commands all",
-  },
-  correctAnswer: "A"
+   answers: [
+     {text: "A: No psykers are permitted in the dark city!", correct: true},
+     {text: "B: There are no rules in the dark city, fool!", correct: false},
+     {text: "C: Thou shall not steal.", correct: false},
+     {text: "D: Asdrubael Vect, the living muse and supreme overlord, commands all", correct: false},
+            ]  
   },
   {
-  question: "Who is the leader of The Kabal Of The Poisoned Tongue?"
-  }
+  question: "Who is the leader of The Kabal Of The Poisoned Tongue?",
+  answers: [
+    {text: "A: Asdrubael Vect", correct: false},
+    {text: "B: Lady Malys", correct: true},
+    {text: "C: Malixian The Mad", correct: false},
+    {text: "D: Baron Sathonyx", correct: false},
+           ]
+  },
+  {
+  question: "What does Kheradruakh translate to in low gothic?",
+  answers: [
+    {text: "A: The end times", correct: false},
+    {text: "B: He who hunts heads", correct: true},
+    {text: "C: She who thirsts", correct: false},
+    {text: "D: The war in heaven", correct: false},
+       ]
+  },
+  {
+  question: "Who is the Queen of Knives?",
+  answers: [
+    {text: "A: Yctria Ghularis", correct: false},
+    {text: "B: Bloody Baroness", correct: false},
+    {text: "C: Kruellagh The Vile", correct: false},
+    {text: "D: Lelith Hesperax", correct: true},
+           ]
+  },
+   {
+  question: "Which Haemonculus Coven does Urien Rakarth lead?",
+  answers: [
+    {text: "A: The Dark Creed", correct: false},
+    {text: "B: The Everspiral", correct: false},
+    {text: "C: The Prophets Of Flesh", correct: true},
+    {text: "D: The Coven Of Twelve", correct: false},
+        ]
+  },
+  {
+  question: "Hexrifles contain a weaponised virus, which is it?",
+  answers: [
+    {text: "A: The Flesh Eater virus", correct: false},
+    {text: "B: The Obliterator virus", correct: false},
+    {text: "C: The Walking pox", correct: false},
+    {text: "D: The Glass plague", correct: true},
+           ]
+  },
+  {
+  question: "The Haemonculus Viscount Syndriq was killed in a humilliating way during the raid of Parocheus, his fellow Haemonculi saw it fit to re-make him as a 'useful' tool, what was Syndriq turned in to?",
+  answers: [
+    {text: "A: Syndriq's sump", correct: true},
+    {text: "B: Syndriq's lump", correct: false},
+    {text: "C: Syndriq's hammer", correct: false},
+    {text: "D: Syndriq's rack", correct: false},
+           ]
+  },
+  {
+  question: "An Aeldari maiden world was stolen from realspace by a combined effort of several Haemonculus covens lead by Urien Rakarth, which planet was it?",
+  answers: [
+    {text: "A: Cadia", correct: false},
+    {text: "B: Lethidia", correct: true},
+    {text: "C: Armageddon", correct: false},
+    {text: "D: Kronus", correct: false},
+    ]
+  },
+  {
+  question: "Who holds the title 'Executioner' within the Incubi order?",
+  answers: [
+    {text: "A: Morr", correct: false},
+    {text: "B: Bechareth", correct: false},
+    {text: "C: Drazhar", correct: true},
+    {text: "D: The Dark Father", correct: false},
+    ]
+  },
+  {
+    question: "How many arms does Kheradruakh have?",
+  answers: [
+    {text: "A: 4", correct: true},
+    {text: "B: 2", correct: false},
+    {text: "C: 1", correct: false},
+    {text: "D: 3", correct: false},
+    ]
+  },
 ]
+
+const questionsRandomized = questions.sort(() => Math.random() - 0.5); //randomizes the questions array. Need to make sure this works right.
+
+const questionElement = document.getElementById('question'); //holds the questions text
+const answerButtonsElement = document.getElementById('answer-buttons'); //each button will hold a answer for the given question
+const nextButton = document.getElementById('next-btn'); //button to submit answer and move onto next question
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  showQuestion();
+}
+
+function showQuestion() {
+  let currentQuestion = questionsRandomized[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+ currentQuestion.answers.forEach(answer => {
+    const button = document.createElement('button');
+    button.innerHTML = answer.text;
+    button.classList.add('btn');
+    answerButtonsElement.appendChild(button);
+});
+}
+
+
+
+
+
+
+console.log(questionsRandomized);
+
+
+
+startQuiz();
+
+
+
+
+
